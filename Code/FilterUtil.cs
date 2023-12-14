47328b79864c6bc2483851a9c1b49e344ec748e2
U-ACER-LAPTOP\stoik <stoiko.todorov@gmail.com>
2023-12-14 16:28:21 +0200

    Support array of lists in filters.

--- Code/FilterUtil.cs
+++ Code/FilterUtil.cs
@@ -11,13 +11,28 @@ public static void CreateAll( object o, out List<IList> all ) {
     all = new List<IList>();
     FieldInfo [] fields = o.GetType().GetFields();
     foreach ( FieldInfo fi in fields ) {
-        Type fldType = fi.FieldType;
-        if( fldType.IsGenericType
-                && fldType.GetGenericTypeDefinition() == typeof( List<> )
-                && fldType != typeof( List<IList> ) ) {
-            var list = Activator.CreateInstance( fldType );
+        Type ft = fi.FieldType;
+        if( ft.IsGenericType
+                && ft.GetGenericTypeDefinition() == typeof( List<> )
+                && ft != typeof( List<IList> ) ) {
+            var list = Activator.CreateInstance( ft );
             fi.SetValue( o, list );
             all.Add( ( IList )list );
+            //Qonsole.Log( $"Creating list {fi.Name}" );
+        }
+        if( ft.IsArray ) {
+            ft = fi.FieldType.GetElementType();
+            if( ft.IsGenericType
+                    && ft.GetGenericTypeDefinition() == typeof( List<> )
+                    && ft != typeof( List<IList> ) ) {
+                Array a = ( Array )fi.GetValue( o );
+                for ( int i = 0; i < a.Length; i++ ) {
+                    var list = Activator.CreateInstance( ft );
+                    a.SetValue( list, i );
+                    all.Add( ( IList )list );
+                    //Qonsole.Log( $"Creating list {fi.Name}[{i}]" );
+                }
+            }
         }
     }
 }
