856e5b04482578dd5a9dbb29cc9923dd9f3ab432
U-ACER-LAPTOP\stoik <stoiko.todorov@gmail.com>
2024-03-11 13:33:30 +0200

    Moved to the 'experimental' navigation with states.

--- Code/Game_sv.cs
+++ Code/Game_sv.cs
@@ -24,7 +24,7 @@ static bool SvShowAvoidance_kvar = false;
 static bool SvShowCharge_kvar = false;
 #endif
 
-static int ChickenBit_kvar = 0;
+//static int ChickenBit_kvar = 0;
 
 public void PostLoadMap() {
     // precache some paths (i.e. between structures)
@@ -50,7 +50,7 @@ public void PostLoadMap() {
     }
 }
 
-public void TickServerExperimental() {
+public void TickServer() {
     void charge( int z, int zEnemy ) {
         pawn.atkFocus[z] = ( byte )zEnemy;
         // change route segment before next movement lerp so we have the proper position
@@ -153,198 +153,6 @@ public void TickServerExperimental() {
     }
 
     DebugDrawOrigins();
-    foreach ( var z in pawn.filter.no_garbage ) {
-        pawn.mvEnd_tx[z] = ToTx( pawn.mvEnd[z] );
-    }
-}
-
-public void TickServer() {
-    if ( ChickenBit_kvar == 0 ) {
-        TickServerExperimental();
-        return;
-    }
-
-    //if ( ChickenBit_kvar == 1 ) {
-    //    Gym_TickServer_v2();
-    //    return;
-    //}
-
-    int getDuration( int z ) {
-        float segmentDist = ( pawn.mvEnd[z] - pawn.mvStart[z] ).magnitude;
-        return ( 60 * ToTx( segmentDist ) / pawn.Speed( z ) * 1000 ) >> FRAC_BITS;
-    }
-
-    Vector2 avoidStructure( int team, Vector2 v0, Vector2 v1 ) {
-        Vector2 ab = v1 - v0;
-        float sq = ab.sqrMagnitude;
-        if ( sq < 0.5f ) {
-            return v1;
-        }
-
-        float dvLen = Mathf.Sqrt( sq );
-        Vector2 abn = ab / dvLen;
-
-        float min = 9999999;
-
-        foreach ( var z in pawn.filter.structures ) {
-            // enemy structures are not avoided, get attacked instead
-            if ( pawn.team[z] != team ) {
-                continue;
-            }
-
-            Vector2 c = pawn.mvPos[z];
-            Vector2 ac = c - v0;
-            Vector2 bc = c - v1;
-
-            float acac = Vector2.Dot( ac, ac );
-
-            // only care about the closest structure intersecting the path
-            if ( acac >= min ) {
-                continue;
-            }
-
-            float sqDist;
-
-            float e = Vector2.Dot( ac, ab );
-            if ( e <= 0 ) {
-                sqDist = Vector2.Dot( ac, ac );
-            } else {
-                float f = Vector2.Dot( ab, ab );
-                if ( e >= f ) {
-                    sqDist = Vector2.Dot( bc, bc );
-                } else {
-                    sqDist = acac - e * e / f;
-                }
-            }
-
-            const float avoidRadius = 1;
-
-            // this structure doesn't intersect the path
-            if ( sqDist > avoidRadius * avoidRadius ) {
-                continue;
-            }
-
-            Vector2 p = Vector2.Perpendicular( abn );
-
-            float sign = Vector2.Dot( ac, p ) >= 0 ? -1 : 1;
-            v1 = c + sign * p * avoidRadius * 1.25f;
-            min = acac;
-
-#if UNITY_STANDALONE || SDL
-            if ( SvShowAvoidance_kvar ) {
-                SingleShot.Add( dt => {
-                    float sz = Draw.hexPixelSize / 4;
-                    Hexes.DrawHexWithLines( Draw.GTS( c ), sz, Color.green );
-                    Hexes.DrawHexWithLines( Draw.GTS( v1 ), sz, Color.white );
-                    QGL.LateDrawLine( Draw.GTS( v0 ), Draw.GTS( v1 ), color: Color.green );
-                }, duration: 3 );
-            }
-#endif
-        }
-
-        return v1;
-    }
-
-    pawn.UpdateFilters();
-    RegisterIntoGrids();
-
-    foreach ( var z in pawn.filter.idling ) {
-        List<byte> enemies = pawn.filter.enemies[pawn.team[z]];
-        foreach ( var zEnemy in enemies ) {
-            if ( ! pawn.IsStructure( zEnemy ) ) {
-                continue;
-            }
-
-            GetCachedPathEndPos( z, zEnemy, out List<int> path );
-
-            if ( path.Count > 2 ) {
-                // push the source position on the side
-                // so the path is properly split in the same hex when spawning units
-
-                // FIXME: check if spawned on a hex split by a zone delimiter
-                // FIXME: and pick a hex on the proper side
-
-                Vector2 snapA = AxialToV( VToAxial( pawn.mvPos[z] ) );
-                Vector2 snapB = AxialToV( VToAxial( pawn.mvPos[zEnemy] ) );
-
-                float dx = snapA.x - snapB.x;
-                if ( dx * dx < 0.0001f ) {
-                    snapA.x += Mathf.Sign( pawn.mvPos[z].x - snapA.x );
-                }
-
-                float dy = snapA.y - snapB.y;
-                if ( dy * dy < 0.0001f ) {
-                    snapA.y += Mathf.Sign( pawn.mvPos[z].y - snapA.y );
-                }
-
-                //SingleShot.Add( dt => {
-                //    Hexes.DrawHexWithLines( Draw.GameToScreenPosition( snapA ),
-                //                                            Draw.hexPixelSize / 4, Color.white );
-                //}, duration: 3 );
-
-                GetCachedPathVec( snapA, pawn.mvPos[zEnemy], out path );
-            }
-
-            if ( path.Count > 1 ) {
-
-                // trigger movement both on the server and the client
-                // by setting movement target and arrival time
-                pawn.navFocus[z] = zEnemy;
-
-                // FIXME: is this redundant?
-                pawn.mvStart[z] = pawn.mvPos[z];
-                pawn.mvEnd[z] = avoidStructure( pawn.team[z], pawn.mvStart[z], HexToV( path[1] ) );
-                pawn.mvStartTime[z] = ZServer.clock;
-                pawn.mvEndTime[z] = ZServer.clock + getDuration( z );
-
-                DebugDrawPath( path );
-                break;
-            }
-        }
-    }
-
-    foreach ( var z in pawn.filter.no_idling ) {
-
-        if ( ! pawn.MvLerp( z, ZServer.clock ) ) {
-            // still lerping
-            //SingleShot.Add( dt => {
-            //    Hexes.DrawHexWithLines( Draw.GameToScreenPosition( pawn.mvPos[z] ) + Vector2.one,
-            //                                                Draw.hexPixelSize * 1.25f, Color.white );
-            //}, duration: 1 );
-            continue;
-        }
-
-        // FIXME: make a filter for all 'currently navigating' pawns
-        if ( pawn.mvEnd_tx[z] == 0 ) {
-            continue;
-        }
-
-        if ( GetCachedPathEndPos( z, pawn.navFocus[z], out List<int> path ) <= 1 ) {
-            // no path to target or target reached
-            Destroy( z );
-            continue;
-        }
-
-        pawn.mvStart[z] = pawn.mvEnd[z];
-        pawn.mvEnd[z] = avoidStructure( pawn.team[z], pawn.mvStart[z], HexToV( path[1] ) );
-
-        int leftover = Mathf.Max( 0, ZServer.clock - pawn.mvEndTime[z] );
-        pawn.mvStartTime[z] = ZServer.clock;
-        pawn.mvEndTime[z] = pawn.mvStartTime[z] + getDuration( z );
-
-        if ( leftover > 0 ) {
-            // advance on the next segment if there is time left from the tick
-            if ( ! pawn.MvLerp( z, pawn.mvStartTime[z] + leftover ) ) {
-                pawn.mvStart[z] = pawn.mvPos[z];
-                pawn.mvEndTime[z] -= leftover;
-            }
-        }
-
-        DebugDrawPath( path );
-    }
-
-    DebugDrawOrigins();
-
     foreach ( var z in pawn.filter.no_garbage ) {
         pawn.mvEnd_tx[z] = ToTx( pawn.mvEnd[z] );
     }
